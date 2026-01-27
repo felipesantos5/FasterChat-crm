57813dac378b283081d573fb486b70e859c856ee
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2026-01-26 22:42:28 -0300

    feat: remove md project

--- backend/update_groups.sql
+++
@@ -1,9 +0,0 @@
--- Atualiza todos os clientes que têm @g.us no telefone para isGroup = true
-UPDATE customers 
-SET is_group = true 
-WHERE phone LIKE '%@g.us%' AND is_group = false;
-
--- Verifica quantos foram atualizados
-SELECT COUNT(*) as grupos_atualizados 
-FROM customers 
-WHERE phone LIKE '%@g.us%';
